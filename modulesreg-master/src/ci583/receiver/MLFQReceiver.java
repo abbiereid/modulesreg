b7df06d80cb23a4f6134f4804f8bfd9553193d12
abbie <abbiereid222@gmail.com>
2024-03-25 17:20:36 +0000

    MLFQ Working

--- modulesreg-master/src/ci583/receiver/MLFQReceiver.java
+++ modulesreg-master/src/ci583/receiver/MLFQReceiver.java
@@ -22,14 +22,18 @@ public class MLFQReceiver extends ModRegReceiver {
     /**
      * Constructs a multi-level feedback queue receiver. The constructor needs to call the constructor of the
      * superclass then initialise the two lists for young and old processes.
+     *
      * @param quantum
      */
     public MLFQReceiver(long quantum) {
-      super(quantum);
+        super(quantum);
+        this.OLD = new ArrayList<>();
+        this.YOUNG = new ArrayList<>();
     }
 
     /**
      * Adds a new process to the list of young processes.
+     *
      * @param m
      */
     @Override
@@ -40,24 +44,72 @@ public class MLFQReceiver extends ModRegReceiver {
     /**
      * Schedule the module registration processes. This method needs to:
      * + create an empty list which will hold the completed processes. This will be the
-     *   return value of the method.
+     * return value of the method.
      * + while one of the queues is not empty:
-     *   - if the list of YOUNG processes is not empty, take the next process and get its State.
-     *   - if the state is NEW, start the process then sleep for QUANTUM milliseconds
-     *     then put the process at the back of the list of OLD processes.
-     *   - if the state is TERMINATED, add it to the results list.
-     *   - if the state is anything else then interrupt the process to wake it up then
-     *     sleep for QUANTUM milliseconds, then put the process at the back of the queue.
+     * - if the list of YOUNG processes is not empty, take the next process and get its State.
+     * - if the state is NEW, start the process then sleep for QUANTUM milliseconds
+     * then put the process at the back of the list of OLD processes.
+     * - if the state is TERMINATED, add it to the results list.
+     * - if the state is anything else then interrupt the process to wake it up then
+     * sleep for QUANTUM milliseconds, then put the process at the back of the queue.
+     * <p>
+     * - if the list of YOUNG processes is empty, do the same except take the process from the
+     * list of OLD processes and, after it does its 'work' put it at the end of the list of
+     * YOUNG processes.
+     * + when both lists are empty, return the list of completed processes.
      *
-     *   - if the list of YOUNG processes is empty, do the same except take the process from the
-     *     list of OLD processes and, after it does its 'work' put it at the end of the list of
-     *     YOUNG processes.
-     *  + when both lists are empty, return the list of completed processes.
      * @return
      */
     @Override
     public List<ModuleRegister> startRegistration() {
-      ArrayList<ModuleRegister> results = new ArrayList<>();
-      return results;
+        ArrayList<ModuleRegister> results = new ArrayList<>();
+
+        while (!YOUNG.isEmpty() || !OLD.isEmpty()) { //whilst both lists are not empty
+            if (!YOUNG.isEmpty()) { //if the list of YOUNG processes is not empty
+                ModuleRegister process = YOUNG.remove(0); //take the next process
+                ModuleRegister.State state = process.getState(); //get the state of the process
+                if (state == ModuleRegister.State.NEW) { //if the state is NEW
+                    process.start(); //start the process
+                    try { // sleep for QUANTUM milliseconds
+                        Thread.sleep(QUANTUM);
+                    } catch (InterruptedException ignored) {
+                    }
+                    OLD.add(process); //put the process at the back of the list of OLD processes
+                } else if (state == ModuleRegister.State.TERMINATED) { //if the state is TERMINATED
+                    results.add(process); //add it to the finished results list
+                } else { //if the state is anything else
+                    process.interrupt(); //interrupt the process to wake it up
+                    try { // sleep for QUANTUM milliseconds
+                        Thread.sleep(QUANTUM);
+                    } catch (InterruptedException ignored) {
+                    }
+                    OLD.add(process); //put the process at the back of the list of OLD processes
+                }
+            }
+            if (!OLD.isEmpty()){
+                ModuleRegister process = OLD.remove(0); //take the next process from the list of OLD processes
+                ModuleRegister.State state = process.getState(); //get the state of the process
+                if (state == ModuleRegister.State.NEW) { //if the state is NEW
+                    process.start(); //start the process
+                    try { // sleep for QUANTUM milliseconds
+                        Thread.sleep(QUANTUM);
+                    } catch (InterruptedException ignored) {
+                    }
+                    YOUNG.add(process); //put the process at the back of the list of YOUNG processes
+                } else if (state == ModuleRegister.State.TERMINATED) { //if the state is TERMINATED
+                    results.add(process); //add it to the finished results list
+                } else { //if the state is anything else
+                    process.interrupt();
+                    try { // sleep for QUANTUM milliseconds
+                        Thread.sleep(QUANTUM);
+                    } catch (InterruptedException ignored) {
+                    }
+                    YOUNG.add(process); //put the process at the back of the list of YOUNG processes
+                }
+            }
+        }
+        return results; //return the list of completed processes
     }
+
 }
+
